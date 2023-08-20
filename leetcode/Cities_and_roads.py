bc2f0b6efcfcdc685205510cbd9f79ba309ec33e
tolossamuel <tolossamuel1@gmail.com>
2023-08-20 23:42:41 +0300

    Cities and roads

--- leetcode/Cities_and_roads.py
+++ leetcode/Cities_and_roads.py
@@ -1,1 +1,8 @@
-x1	
+number = int(input())
+adjacency_list = [list(map(int, input().split())) for _ in range(number)]
+counter = 0
+for y in range(number):
+    for x in range(len(adjacency_list[y])):
+        if adjacency_list[y][x] == 1:
+            counter += 1
+print(counter//2)
