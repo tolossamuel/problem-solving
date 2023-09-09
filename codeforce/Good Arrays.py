287602c6165f280d08a3734d21488d4ac67a24f5
tolossamuel <tolossamuel1@gmail.com>
2023-09-09 19:29:19 +0300

    a2sv contest

--- codeforce/Good Arrays.py
+++ codeforce/Good Arrays.py
@@ -1,12 +1,16 @@
-num = int(input())
-while num > 0:
-    num -=1
-    n, a = int(input()), input()
-    ans, sum_of, m = 0, 0, {}
-    for c in a:
-        sum_of += (int(c) - 1)
-        if sum_of == 0:
-            ans += 1
-        ans += m.get(sum_of, 0)
-        m[sum_of] = m.get(sum_of, 0) + 1
-    print(ans)
+def solve():
+    for i in range(int(input())):
+        n = int(input())
+        arr =  list(map(int, input().split()))
+
+        s = sum(arr)
+
+        # total sum of b not equal to sum of s
+        #  is it possible to make sum using n items and also a[i]!=b[i]
+        
+        # if n = 1 ans = 1
+        # if len(arr) == let(set(arr))
+        
+        
+if __name__ == "__main__":
+    solve()
