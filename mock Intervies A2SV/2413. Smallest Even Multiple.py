72b19598542c3337314c4f354b952ec3eabc26f5
tolossamuel <tolossamuel1@gmail.com>
2023-11-10 15:36:15 +0300

    a2sv

---
+++ mock Intervies A2SV/2413. Smallest Even Multiple.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def smallestEvenMultiple(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 2:
+            return 2
+        commen = n*2
+        while((commen/min(2,n)) %2 ==0 and commen/min(2,n) %n ==0):
+            commen /= min(2,n)
+        return commen
