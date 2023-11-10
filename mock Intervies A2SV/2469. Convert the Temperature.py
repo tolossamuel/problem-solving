5acb2acc27bf11e28683ae42bec7c2f6e5062bcb
tolossamuel <tolossamuel1@gmail.com>
2023-11-10 15:16:59 +0300

    a2sv

---
+++ mock Intervies A2SV/2469. Convert the Temperature.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        digree = []
+        digree.append(celsius+273.15)
+        digree.append( (celsius * 1.80 + 32.00))
+        return digree
+        
+	
