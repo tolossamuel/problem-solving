69d186c8cf02ded86831e5c6e8b68b2af986d267
tolossamuel <tolossamuel1@gmail.com>
2023-12-11 17:27:50 +0300

    leetcode

--- leetcode/950. Reveal Cards In Increasing Order.py
+++ leetcode/950. Reveal Cards In Increasing Order.py
@@ -1,15 +1,44 @@
-class Solution(object):
-    def deckRevealedIncreasing(self, deck):
-        """
-        :type deck: List[int]
-        :rtype: List[int]
-        """
-        n = len(deck)
-        deck.sort()
-        result = [0] * n
-        indexs = deque(range(n))
-        for card in deck:
-            result[indexs.popleft()] = card
-            if indexs:
-                indexs.append(indexs.popleft())
-        return result
+# class Solution(object):
+#     def deckRevealedIncreasing(self, deck):
+#         """
+#         :type deck: List[int]
+#         :rtype: List[int]
+#         """
+#         n = len(deck)
+#         deck.sort()
+#         result = [0] * n
+#         indexs = deque(range(n))
+#         for card in deck:
+#             result[indexs.popleft()] = card
+#             if indexs:
+#                 indexs.append(indexs.popleft())
+#         return result
+
+def happy(n,b,arr):
+            if n == 1:
+                
+                return True
+            elif n <= 3:
+               
+                return False
+            else:
+                m = str(n)
+                sumOf = 0
+                for i in range(len(m)):
+                    sumOf += int(m[i])**2
+                print(sumOf)
+                
+                
+                if  sumOf in arr:
+                    print(sumOf,n,b)
+                    print(True)
+                    checker = True
+                    
+                    return False
+                else:
+                    arr.append(sumOf)
+                  
+                    return happy(sumOf,b,arr)
+
+name = happy(5,5,[])
+print(name)
--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -1,10 +1,12 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        listt=[]
-        for i in range(len(nums)-1):
-            for y in range(i+1,len(nums)):
-                if(nums[i]+nums[y]==target):
-                    listt.append(i)
-                    listt.append(y)
-                    return listt
-        return -1
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         listt=[]
+#         for i in range(len(nums)-1):
+#             for y in range(i+1,len(nums)):
+#                 if(nums[i]+nums[y]==target):
+#                     listt.append(i)
+#                     listt.append(y)
+#                     return listt
+#         return -1
+
+
