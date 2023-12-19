cf41be9d4cb44da8bb1fdbea1a83952263ae17f4
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2023-12-19 22:09:03 +0300

    Added solution for Find Three Consecutive Integers That Sum to a Given Number

---
+++ problem-solving/A2SV/find-three-consecutive-integers-that-sum-to-a-given-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sumOfThree(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        
+        if num%3 != 0:
+            return []
+        return [((num//3)-1),(num//3),((num//3)+1)]
