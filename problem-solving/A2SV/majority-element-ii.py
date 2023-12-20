a1a6d540407b3f49e96b73166679e86ba858f069
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2023-12-20 23:23:00 +0300

    Added solution for Majority Element II

---
+++ problem-solving/A2SV/majority-element-ii.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        counter ={}
+        for i in nums:
+            counter[i] = 0
+        for i in nums:
+            counter[i] += 1
+        result = set()
+        for i in nums:
+            if counter[i] > (n/3):
+                result.add(i)
+
+        return list(result)
+        
