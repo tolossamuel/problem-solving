e9cd747b446c783925fdc30458a83c6e1c3e2205
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2023-12-24 11:04:31 +0300

    Added solution for Partition Array According to Given Pivot

--- problem-solving/A2SV/partition-array-according-to-given-pivot.py
+++ problem-solving/A2SV/partition-array-according-to-given-pivot.py
@@ -5,39 +5,14 @@ class Solution(object):
         :type pivot: int
         :rtype: List[int]
         """
-        p = 0
-        for i in range(len(nums),0,-1):
-            if nums[i-1] == pivot:
-                p = i-1
-                break
-        left = p-1
-        right = p+1
+        less = []
+        greater = []
         equal = 0
-        while(left >= 0 or right < len(nums)):
-            
-            if left >= 0:
-                
-                if nums[left] > nums[p]:
-                    
-                    nums.insert(p+1, nums[left])  
-                                    
-                    nums.pop(left)
-                    p-= 1
-                elif nums[left] == nums[p]:
-                    equal += 1
-                    nums.insert(p,nums[left])
-                    nums.pop(left)
-            if right < len(nums):
-                if nums[right] < nums[p]:
-                    nums.insert(p-equal,nums[right])
-                    nums.pop(right+1)
-                    p += 1
-                elif nums[right] == nums[p]:
-                    nums.insert(p,nums[right])
-                    nums.pop(right+1)
-                    equal += 1
-                    p+=1  
-            left -= 1
-            right +=1
-         
-        return nums
+        for i in nums:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                greater.append(i)
+            else:
+                equal += 1
+        return (less+ [pivot] * equal + greater)
