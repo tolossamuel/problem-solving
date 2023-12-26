254d2a62d07512fcb034e64de3878706c5dca04b
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2023-12-26 14:49:43 +0300

    Added solution for Number of Times Binary String Is Prefix-Aligned

--- problem-solving/A2SV/number-of-times-binary-string-is-prefix-aligned.py
+++ problem-solving/A2SV/number-of-times-binary-string-is-prefix-aligned.py
@@ -5,12 +5,12 @@ class Solution(object):
         :rtype: int
         """
         n = len(flips)
-        zeros = "0"*n
+        counter = 0
         ans = 0
         _max = 0
         for i in range(n):
-            zeros = zeros[:flips[i]-1] + "1" + zeros[flips[i]:]
-            _max = max(_max,flips[i]-1)
-            if zeros[:_max+1] == "1"*(_max+1) and zeros[_max+1:] == "0"*(n-_max-1):
+            _max = max(_max,flips[i])
+            counter += 1
+            if _max == counter :
                 ans += 1
         return ans
