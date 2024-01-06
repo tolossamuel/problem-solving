133c5ac8f305f9d47c8179846e2645b809fba17e
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-01-06 18:22:52 +0300

    Added solution for Sort Colors

--- problem-solving/A2SV/sort-colors.py
+++ problem-solving/A2SV/sort-colors.py
@@ -4,18 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # Initialize pointers for the three colors: red (0), white (1), and blue (2)
-        red, white, blue = 0, 0, len(nums) - 1
-        
-        while white <= blue:
-
-            if nums[white] == 0:
-                nums[red], nums[white] = nums[white], nums[red]
-                red += 1
-                white += 1
-            # If the current element is 1, move the white pointer to the right
-            elif nums[white] == 1:
-                white += 1
+        r,w,b = 0,0 ,len(nums)-1
+        while(w <= b):
+            if nums[w] == 0:
+                nums[r],nums[w] = nums[w],nums[r]
+                w += 1
+                r += 1
+            elif nums[w] == 1:
+                w += 1
             else:
-                nums[white], nums[blue] = nums[blue], nums[white]
-                blue -= 1
+                nums[b],nums[w] = nums[w],nums[b]
+                b-=1
+             
