f90e56ea7c620f3d3e0d710c6cb0399937a1d5ff
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-01-16 11:17:32 -0500

    Added solution for Find All Anagrams in a String

--- problem-solving/A2SV/find-all-anagrams-in-a-string.py
+++ problem-solving/A2SV/find-all-anagrams-in-a-string.py
@@ -1,16 +1,24 @@
+from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         left = 0 
-        right = len(p)-1
-        p = list(p)
-        p.sort()
+        right = len(p)
+        
+        p = Counter(p)
         ans = []
+        dic = defaultdict(int)
+        dic = Counter(s[:right])
         while(right < len(s)):
-            temp = list(s[left:right+1])
-            temp.sort()
-            if temp == p:
+            if dic == p:
                 ans.append(left)
+            dic[s[left]] -= 1
+            dic[s[right]] += 1
+            if dic[s[left]] == 0:
+                dic.pop(s[left])
             left += 1
-            right +=1
+            right += 1
+    
+        if dic == p:
+            ans.append(left)
         return ans
         
