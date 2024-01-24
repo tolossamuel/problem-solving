8f987e0f3576d9b0589ce4a672503f8d16806e80
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-01-24 15:44:04 -0500

    Added solution for Shifting Letters II

---
+++ problem-solving/A2SV/shifting-letters-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        pref = [0]*len(s)
+        for i in shifts:
+            pref[i[0]] += 1 if i[2] == 1 else -1
+            if i[1]+1 < len(s):
+                pref[i[1]+1] += -1 if i[2] == 1 else 1
+        #     print(pref)
+        # print(pref)
+        for i in range(1,len(s)):
+            pref[i] += pref[i-1]
+        ans = []
+        # print(pref)
+        for i in range(len(pref)):
+            x =(ord(s[i])-97 + pref[i])%26
+            ans.append(chr(x+97))
+        # print(ans)
+        return "".join(ans)
