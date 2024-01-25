45b23b662522a6a993aa735a45fef5e8722450d8
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-01-25 10:03:35 -0500

    Added solution for Range Sum Query 2D - Immutable

--- problem-solving/A2SV/range-sum-query-2d-immutable.py
+++ problem-solving/A2SV/range-sum-query-2d-immutable.py
@@ -1,28 +1,18 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        for i in range(len(matrix)):
-            for y in range(1,len(matrix[0])):
-                matrix[i][y] += matrix[i][y-1]
-        for y in range(1,len(matrix)):
-            for i in range(len(matrix[0])):
-                matrix[y][i] += matrix[y-1][i]
-        self.matrix = matrix
-        
+        self.pref = [[0 for i in range(len(matrix[0])+1)] for i in range(len(matrix)+1)]
+        for i in range(1,len(matrix)+1):
+            for y in range(1,len(matrix[0])+1):
+                self.pref[i][y] = matrix[i-1][y-1] + self.pref[i][y-1] + self.pref[i-1][y] - self.pref[i-1][y-1] 
+
+        # for i in self.pref:
+        #     print(i)
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        
-        ans = (self.matrix [row2][col2])
-        # print(ans)
-        if row1 - 1 >= 0:
-            ans -= self.matrix [row1-1][col2]
-            # print(ans,self.matrix [row1-1][col1])
-        if col1 -1 >= 0:
-            ans -= self.matrix [row2][col1-1]
-        if row1-1 >=0  and (col1-1 )>= 0:
-            ans += self.matrix [row1-1][col1-1]
-           
-        return ans
+        # print(row1,col1,row2,col2)
+        # print(self.pref[row2+1][col2+1],self.pref[row1][col2+1] , self.pref[row2+1][col1] , self.pref[row1][col1])
+        return (self.pref[row2+1][col2+1] - self.pref[row1][col2+1] - self.pref[row2+1][col1] + self.pref[row1][col1])
         
 
 
