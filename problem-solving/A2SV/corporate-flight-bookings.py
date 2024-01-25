66866b669befe4206da2a4b0bf876958e36d6dcc
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-01-25 02:57:30 -0500

    Added solution for Corporate Flight Bookings

---
+++ problem-solving/A2SV/corporate-flight-bookings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        pref = [0]*n
+        for i in bookings:
+            pref[i[0]-1] += i[2]
+            if i[1] < len(pref):
+                pref[i[1]] += -i[2]
+        # print(pref)
+        for i in range(1,n):
+            pref[i] += pref[i-1]
+        return pref
