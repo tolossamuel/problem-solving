a0b38e56fb9792820fa5bc51b5710ebad79f4eee
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-02-02 13:24:07 +0300

    Add solution for Reverse Linked List

--- problem-solving/A2SV/leetcode/reverse-linked-list.py
+++ problem-solving/A2SV/leetcode/reverse-linked-list.py
@@ -5,18 +5,17 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head == None:
-            return head
-        if not head.next:
-            return head
-        temp = ListNode(head.val)
-        head = head.next
-        index = head.next
-        while(index):
-            head.next = temp
-            temp = head
-            head = index
-            index = index.next
-        head.next = temp
-   
-        return head
+        # if head == None or head.next == None:
+        #     return head
+        def recurtions(node):
+            if node.next:
+                self.left.next = recurtions(node.next)
+                self.left = self.left.next
+                self.left.next = None
+            # print(node.val)
+            return node
+        dummy = ListNode(0,head)
+        self.left = dummy
+        self.left.next = recurtions(dummy)
+        self.left.next = None
+        return dummy.next
