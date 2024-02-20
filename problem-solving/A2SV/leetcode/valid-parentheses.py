e67ae32ae1f5d8754f37f8ca88edd9c5316ec785
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-02-20 08:00:58 +0300

    Add solution for Valid Parentheses

--- problem-solving/A2SV/leetcode/valid-parentheses.py
+++ problem-solving/A2SV/leetcode/valid-parentheses.py
@@ -1,28 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        dic = defaultdict(int)
+        dic = {
+            ")":"(",
+            "}":"{",
+            "]":"["
+        }
         stack = []
         for i in range(len(s)-1,-1,-1):
-            if s[i] == ")" or s[i] == "}" or s[i] == "]":
-                dic[s[i]] += 1
+            if s[i] in dic:
                 stack.append(s[i])
-            elif s[i] == "(":
-                if dic[")"] > 0 and stack and stack[-1] == ")":
-                    dic[")"] -= 1
+            else:
+                if stack and dic[stack[-1]] == s[i]:
                     stack.pop()
                 else:
                     return False
-            elif s[i] == "{":
-                if dic["}"] > 0 and stack and stack[-1] == "}":
-                    dic["}"] -= 1
-                    stack.pop()
-                else:
-                    return False
-            elif s[i] == "[":
-                if dic["]"] > 0 and stack and stack[-1] == "]":
-                    dic["]"] -= 1
-                    stack.pop()
-                else:
-                    return False
-        return max(dic.values()) == 0 
+        return len(stack) == 0
             
