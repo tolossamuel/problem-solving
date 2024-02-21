06e1d536d9a236b139b4423923dba225a71ec88e
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-02-21 19:50:02 +0300

    Add solution for Time Needed to Buy Tickets

--- problem-solving/A2SV/leetcode/time-needed-to-buy-tickets.py
+++ problem-solving/A2SV/leetcode/time-needed-to-buy-tickets.py
@@ -1,10 +1,10 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         counter = 0
-        i = 0
-        while(tickets[k] != 0):
-            if tickets[i%len(tickets)] != 0:
-                tickets[i%len(tickets)] -= 1
-                counter += 1
-            i += 1
+        for i in range(len(tickets)):
+            if i <= k:
+                counter += min(tickets[i],tickets[k])
+            else:
+                counter += min(tickets[i],tickets[k]-1)
+            
         return counter
