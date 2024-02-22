1f228231cfd6b5b0e84934d413ecd59882a93622
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-02-22 11:47:30 +0300

    Add solution for Number of Recent Calls

--- problem-solving/A2SV/leetcode/number-of-recent-calls.py
+++ problem-solving/A2SV/leetcode/number-of-recent-calls.py
@@ -1,12 +1,13 @@
 class RecentCounter:
 
     def __init__(self):
-        self.dif = []
+        self.dif = deque()
 
     def ping(self, t: int) -> int:
         self.dif.append(t)
-        x = bisect.bisect_left(self.dif, (t-3000))
-        return len(self.dif) - x
+        while(self.dif[0] < (t-3000)):
+            self.dif.popleft()
+        return len(self.dif)
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
 # param_1 = obj.ping(t)
