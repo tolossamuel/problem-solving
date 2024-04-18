02de42f6a9b85fd8c98d090cd3702a4d526055cd
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-04-18 18:34:40 +0300

    Time: 90 ms (60.79%) | Memory: 18.3 MB (66.90%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,24 +1,28 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        indegree = [0] * numCourses
+        color = [0] * numCourses
         pre_request = defaultdict(list)
         for i,j in prerequisites:
-            indegree[i] += 1
-            pre_request[j].append(i)
+            pre_request[i].append(j)
         courseOrder = []
         queue = []
-        # print(indegree,pre_request)
-        for i in range(len(indegree)):
-            if indegree[i] == 0:
-                queue.append(i)
-        queue = deque(queue)
-        while(queue):
-            curr = queue.popleft()
-            courseOrder.append(curr)
-            for i in pre_request[curr]:
-                indegree[i] -= 1
-                if indegree[i] == 0:
-                    queue.append(i)
-        if len(courseOrder) !=  numCourses:
-            return []
+        visited = set()
+        def dfs(course):
+            res = True
+            for i in pre_request[course]:
+                if color[i] == 1:
+                    return False
+                if  color[i] != 2:
+                    
+                    color[i] = 1
+                    res = res and dfs(i)
+                
+            courseOrder.append(course)
+            color[course] = 2
+            return res
+        for i in range(numCourses):
+            if color[i] == 0:
+                if not dfs(i):
+                    return []
         return courseOrder
+
