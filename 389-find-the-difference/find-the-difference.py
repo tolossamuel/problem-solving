2d2dba88ede0947d3e2630574a4327e78cd7927e
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-04-30 19:26:36 +0300

    Time: 33 ms (84.92%) | Memory: 16.6 MB (84.50%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        ans = 0
+        for i in t:
+            ans ^= ord(i)
+        for i in s:
+            ans ^= ord(i)
+        return chr(ans)
