ca72848ae75e510387c725163f1099c792402f9e
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-05-02 14:15:43 +0300

    Time: 653 ms (92.20%) | Memory: 34.9 MB (42.66%) - LeetSync

---
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        xor_ = 0
+        for i in nums:
+            xor_ ^= i
+        k = (2**maximumBit)-1
+        ans = []
+        for i in range(len(nums)-1,-1,-1):
+            ans.append(xor_^k)
+            xor_ ^= nums[i]
+        return ans
+
+
