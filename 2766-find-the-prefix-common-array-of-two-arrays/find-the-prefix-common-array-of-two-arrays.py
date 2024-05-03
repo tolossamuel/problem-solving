5ab41d507a9eb7e7173251de445fa1617864fd10
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-05-03 22:47:54 +0300

    Time: 104 ms (89.96%) | Memory: 16.6 MB (69.28%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -8,7 +8,6 @@ class Solution:
             arr.append(common)
             
             if A[i] != B[i] and (not((1 << B[i]) & visited1) and not((1<<A[i]) & visited2)):
-                print(bin(visited1),bin(visited2),bin(1<<B[i]),bin(1<<A[i]), i)
                 common -= 2
             elif not (((1 << B[i]) & visited1) and ((1<<A[i]) & visited2)):
                 common -= 1
