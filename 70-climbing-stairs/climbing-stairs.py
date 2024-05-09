cb8f6a8cba0143dfd6ce407009b74c25e702c311
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-05-09 18:07:47 +0300

    Time: 34 ms (59.02%) | Memory: 16.6 MB (30.50%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        catch = {}
+        def fib(x):
+            if x <= 1:
+                return x
+            if x in catch:
+                return catch[x]
+            res = fib(x-1) + fib(x-2)
+            catch[x] = res
+            return res
+        return fib(n+1)
