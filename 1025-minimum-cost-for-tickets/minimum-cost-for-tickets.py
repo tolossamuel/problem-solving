a30a30d6139d1a5304804c378eac5ba5d8b68aab
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-05-23 14:31:57 +0300

    Time: 40 ms (81.93%) | Memory: 16.5 MB (87.46%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp = [0]*len(days)
+        dp[-1] = min(costs)
+        for i in range(len(days)-2,-1,-1):
+            next_7_day = days[i] + 6
+            pos = bisect.bisect_right(days,next_7_day)
+            next_7_day = costs[1]
+
+            if pos < len(days):
+                next_7_day += dp[pos]
+            next_30_day = days[i] + 29
+            pos = bisect.bisect_right(days,next_30_day)
+            next_30_day = costs[2]
+            if pos < len(days):
+                next_30_day += dp[pos]
+            dp[i] = min(next_30_day,next_7_day,dp[i+1]+costs[0])
+        return dp[0]
