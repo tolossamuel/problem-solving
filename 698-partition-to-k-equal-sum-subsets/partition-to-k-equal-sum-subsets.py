1be8a1e856d5bd1a20180db9c72f6296987fddb8
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-05-28 23:28:49 +0300

    Time: 73 ms (71.37%) | Memory: 16.5 MB (92.74%) - LeetSync

---
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        
+        
+        if k == 1:
+            return True
+        if k >= 11:
+            return False 
+        s = sum(nums)
+        if s% k != 0:
+            return False
+        h = s//k
+        for i in nums:
+            if i > h:
+                return False
+        visited = set()
+        subset = [0]*k
+        nums.sort(reverse = True)
+        def solve(index):
+            if index >= len(nums):
+                return True
+            for j in range(k):
+                if subset[j] + nums[index] <= h:
+                    subset[j] += nums[index]
+                    if solve(index+1):
+                        return True
+                    subset[j] -= nums[index]
+                    if subset[j] == 0:
+                        break
+            return False
+      
+        return solve(0)
