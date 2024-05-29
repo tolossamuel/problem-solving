c2ce0b3b8ed81627a6e3a1f7ad23857ad7bc10ab
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-05-29 17:35:46 +0300

    Time: 420 ms (11.01%) | Memory: 27.4 MB (27.52%) - LeetSync

---
+++ 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumXORSum(self, nums1: List[int], nums2: List[int]) -> int:
+        cache = {}
+        def solve(index,visited):
+            if index >= len(nums1):
+                return 0
+            if (index,visited) in cache:
+                return cache[(index,visited)]
+            min_ans = float("inf")
+
+            for i in range(len(nums2)):
+                if (visited & (1 << i)) == 0:
+                    visited ^= (1<<i)
+                    min_ans = min(
+                        min_ans,
+                        solve(index+1,visited) + ( nums1[index]^nums2[i])
+                        
+                    )
+                    visited ^= (1<<i)
+            cache[(index,visited)] = min_ans
+            return min_ans
+        ans = solve(0,0)
+        return ans if ans != float("inf") else 1
