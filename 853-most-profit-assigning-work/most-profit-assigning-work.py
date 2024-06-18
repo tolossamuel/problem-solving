d6783b91186f94e646f06f19538a2dc3159e70c5
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-06-18 15:04:48 +0300

    Time: 281 ms (51.33%) | Memory: 19.5 MB (74.98%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        profit_difficulty_pairs = list(zip(profit, difficulty))
+        profit_difficulty_pairs.sort(key=lambda pair: pair[1])
+        sorted_profit = [pair[0] for pair in profit_difficulty_pairs]
+        difficulty.sort()
+        _max = sorted_profit[0]
+        for x in range(1,len(sorted_profit)):
+            sorted_profit[x] = max(sorted_profit[x],_max)
+            _max = max(sorted_profit[x],_max)
+      
+        _sum = 0
+        for x in worker:
+            index = bisect.bisect_right(difficulty,x)
+            if index == 0:
+                continue
+            index -= 1
+            _sum += sorted_profit[index]
+
+        return _sum
