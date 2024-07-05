6031af5288b6803485383286a89f38e83b27c592
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-07-05 10:01:35 +0300

    Time: 321 ms (30.59%) | Memory: 16.5 MB (55.07%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,12 +1,5 @@
 class Solution:
     def fib(self, n: int) -> int:
-        catch = {}
-        def fib(x):
-            if x <= 1:
-                return x
-            if x in catch:
-                return catch[x]
-            res = fib(x-1) + fib(x-2)
-            catch[x] = res
-            return res
-        return(fib(n))
+        if n <= 1:
+            return n
+        return self.fib(n-1) + self.fib(n-2)
