ac925ac71bc4172d4f042c57fc90f10ac0a9a09e
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-07-24 15:47:27 +0300

    Time: 964 ms (76.63%) | Memory: 26.6 MB (18.48%) - LeetSync

---
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        hash = defaultdict(list)
+        for x in nums:
+            _sum = 0
+            curr = str(x)
+            for y in curr:
+                _sum *= 10
+                _sum += mapping[int(y)]
+            hash[_sum].append(int(x))
+        key = list(hash.keys())
+        key.sort()
+        ans = []
+        for x in key:
+            ans += hash[x]
+
+        return ans
