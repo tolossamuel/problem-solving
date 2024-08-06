7102d935ecbe76746db570f715f57db0ccaf23bc
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-08-06 12:59:08 +0300

    Time: 416 ms (49.80%) | Memory: 29.9 MB (8.06%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -1,24 +1,29 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        dic = defaultdict(list)
+        heap = defaultdict(list)
         degree = [0] * n
-        root = set(list(range(n)))
         for x,y in edges:
-            dic[x].append(y)
-            degree[y] += 1
-            dic[y].append(x)
+            heap[x].append(y)
+            heap[y].append(x)
             degree[x] += 1
-        q = deque([x for x in range(n) if degree[x] == 1])
-        # print(q)
-        # print(dic)
+            degree[y] += 1
+        
+        root = set(list(range(n)))
+        start = deque([x for x in range(len(degree)) if degree[x] == 1])
+        print(start)
+        ans = deque([])
         while(len(root) > 2):
-            length = len(q)
+            length = len(start)
             for _ in range(length):
-                curr = q.popleft()
-                root.remove(curr)
-                for i in dic[curr]:
-                    degree[i] -= 1
-                    if degree[i] == 1:
-                        q.append(i)
-        # print(root)
+                current = start.popleft()
+                root.remove(current)
+                
+                for x in heap[current]:
+                  
+                    degree[x] -= 1
+                    if degree[x] == 1:
+                        start.append(x)
+                   
+  
+
         return list(root)
