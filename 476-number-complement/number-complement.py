745c09c1a9ecfca5e723798ef010f566407e3d67
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-08-22 10:49:44 +0300

    Time: 42 ms (9.65%) | Memory: 16.4 MB (58.08%) - LeetSync

--- 476-number-complement/number-complement.py
+++ 476-number-complement/number-complement.py
@@ -1,11 +1,6 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        ans = log(num)/log(2)
-        
-        ans = ceil(ans)
-        # print(ans)
-        if num == (2**ans):
-            ans += 1
-        ans = (2**ans) - 1
-        ans = ans^num
-        return ans
+        length = len(bin(num))-2
+        print(length,bin(num))
+        max_num = (2**length)-1
+        return max_num^num
