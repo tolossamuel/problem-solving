62de94d68ef8f1aaf78177269e5ac4d2193544a3
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-09-07 02:14:39 +0300

    Created 'D - Decide Your Division' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 06-Sep-2024/D - Decide Your Division 213177.py
@@ -0,0 +1,28 @@
+# Problem: D - Decide Your Division - https://codeforces.com/gym/543431/problem/D
+
+def solve(n):
+    start = 1
+    opperation = 0
+    
+    while(n > 1):
+        if n%2 == 0:
+            n //= 2
+            opperation += 1
+        elif (n%3) == 0:
+            n *= 2
+            n //= 3
+            opperation += 1
+        elif (n%5) == 0:
+            n *= 4
+            n //= 5
+            opperation += 1
+        else:
+            opperation = -1
+            break
+        
+    print(opperation)
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    solve(n)
+          
