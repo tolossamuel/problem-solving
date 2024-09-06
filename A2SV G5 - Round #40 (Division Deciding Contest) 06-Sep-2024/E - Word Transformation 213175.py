61704bb6fd52558a3dfe6b4cc7aaae87de12c343
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-09-07 02:13:47 +0300

    Created 'E - Word Transformation' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 06-Sep-2024/E - Word Transformation 213175.py
@@ -0,0 +1,35 @@
+# Problem: E - Word Transformation - https://codeforces.com/gym/543431/problem/E
+
+def solve(s,d):
+    heap = {}
+    y = len(d) - 1
+    x = len(s) - 1
+    while(x >= 0 and y >= 0):
+        # print(x,y,'========')
+        if d[y] == s[x]:
+            if (y == len(d)-1):
+                
+                y -= 1
+                x -= 1
+            else:
+                if (d[y] not in heap):
+                    y -= 1
+                    x -= 1
+                else:
+                    y = len(d) - 1
+        else:
+            if (s[x] not in heap):
+                heap[s[x]] = 0
+            heap[s[x]] += 1
+            x -= 1
+        # print(x,y)
+    if y < 0:
+        print("YES")
+    else:
+        print("NO")
+                    
+t = int(input())
+for _ in range(t):
+    s = input().split()
+    solve(s[0],s[1])  
+    
