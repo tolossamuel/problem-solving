18ac0b697e3f1fa16d780fded6a82ad798f2a909
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-09-07 02:14:59 +0300

    Created 'A - Memory and Crow' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 06-Sep-2024/A - Memory and Crow 213180.py
@@ -0,0 +1,24 @@
+# Problem: A - Memory and Crow - https://codeforces.com/gym/543431/problem/A
+
+n = int(input())
+
+l = list(map(int,input().split()))
+b = [l[-1]]
+
+odd = b[0] if (n-1)%2 == 1 else 0
+even = b[0] if (n-1)%2 == 0 else 0
+
+for i in range(len(l)-2,-1,-1):
+    if (i%2 == 0):
+        curr = l[i] + odd - even
+        even += curr
+        b.append(curr)
+    else:
+        curr = l[i] - odd + even
+
+        odd += curr
+        b.append(curr)
+
+    
+        
+print(*b[::-1])
