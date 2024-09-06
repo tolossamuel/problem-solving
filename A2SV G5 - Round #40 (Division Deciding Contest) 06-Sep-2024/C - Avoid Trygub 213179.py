818e8a6d50a68f103414b89aceeb933662b6f71a
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-09-07 02:14:45 +0300

    Created 'C - Avoid Trygub' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 06-Sep-2024/C - Avoid Trygub 213179.py
@@ -0,0 +1,10 @@
+# Problem: C - Avoid Trygub - https://codeforces.com/gym/543431/problem/C
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    s = list(s)
+    s.sort()
+    s = "".join(s)
+    print(s)
