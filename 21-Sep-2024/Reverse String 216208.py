450d3c6806a428cda4b891557f7b303671b46b1e
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-09-21 19:17:48 +0300

    Created 'Reverse String' by A2SV Hub

---
+++ 21-Sep-2024/Reverse String 216208.py
@@ -0,0 +1,17 @@
+# Problem: Reverse String - https://leetcode.com/problems/reverse-string/description/
+
+class Solution:
+    def recurtions(self,x,y,s):
+        if x >= y:
+            return 
+        s[x],s[y] = s[y],s[x]
+        self.recurtions(x+1,y-1,s)
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
+        self.recurtions(0,len(s)-1,s)
+        
+        
+        
