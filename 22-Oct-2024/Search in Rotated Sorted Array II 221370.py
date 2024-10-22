7114ce0302c76496a1914e25dfabae713f74ecbc
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2024-10-22 17:52:29 +0300

    Created 'Search in Rotated Sorted Array II' by A2SV Hub

---
+++ 22-Oct-2024/Search in Rotated Sorted Array II 221370.py
@@ -0,0 +1,7 @@
+# Problem: Search in Rotated Sorted Array II - https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if(target in nums):
+            return True
+        return False
